438c28b4085adf5d457443bb9b9ed9030450161e
Connor McLaughlan <venom1539@gmail.com>
2022-10-17 19:05:36 -0600

    First draft finished

--- init_tables.sql
+++ init_tables.sql
@@ -25,12 +25,47 @@ CREATE TABLE sessions (
 );
 
 --listen(uid, sno, sid, cnt)
+CREATE TABLE listen (
+    uid CHAR(25),
+    sno INT,
+    sid INT,
+    cnt INT,
+    PRIMARY KEY (uid, sno, sid),
+    FOREIGN KEY (uid, sno, sid)
+);
 
 --playlists(pid, title, uid)
+CREATE TABLE playlists (
+    pid INT,
+    title TEXT,
+    uid CHAR(25),
+    PRIMARY KEY (pid, uid),
+    FOREIGN KEY (uid)
+);
 
 --plinclude(pid, sid, sorder)
+CREATE TABLE plinclude (
+    pid INT,
+    sid INT,
+    sorder INT,
+    PRIMARY KEY (pid, sid, sorder),
+    FOREIGN KEY (pid, sid)
+);
 
 --artists(aid, name, nationality, pwd)
+CREATE TABLE artists (
+    aid CHAR(25),
+    name TEXT,
+    nationality TEXT,
+    pwd TEXT,
+    PRIMARY KEY (aid)
+);
 
 --perform(aid, sid)
+CREATE TABLE perform (
+    aid CHAR(25),
+    sid INT,
+    PRIMARY KEY (aid, sid),
+    FOREIGN KEY (aid, sid)
+);
 
