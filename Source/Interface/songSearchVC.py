1fe505944bbebab6dd28a610110a3b2f2e50cf9a
Connor McLaughlan <cmclaughlan15@gmail.com>
2022-10-29 16:31:15 -0600

    Treeview working. Placeholder data used.

--- Source/Interface/songSearchVC.py
+++ Source/Interface/songSearchVC.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from Application.application import *
+from Application.dataObjects import *
 
 class SongSearchVC(tk.Frame):
     def __init__(self, app, parent=None):
@@ -13,12 +14,18 @@ class SongSearchVC(tk.Frame):
         self.rowconfigure(0, weight=1)
 
         self.search = tk.StringVar()
+        self.searchView = None
 
         self.create_view()
 
     def create_view(self):
-        searchView = ttk.Treeview(self, columns=('id', 'Title', 'Duration'))
-        searchView.grid(column=0, row=0, columnspan=3)
+        self.searchView = ttk.Treeview(self, columns=('id', 'Title', 'Duration', 'Type'))
+        self.searchView.heading('id', text='id')
+        self.searchView.heading('Title', text='Title')
+        self.searchView.heading('Duration', text='Duration')
+        self.searchView.heading('Type', text='Type')
+        self.searchView['show'] = 'headings'  # Remove empty first column
+        self.searchView.grid(column=0, row=0, columnspan=3)
 
         search_entry = tk.Entry(self, width=15, textvariable=self.search)
         search_entry.grid(column=0, row=1, columnspan=2, sticky=(tk.W, tk.E))
@@ -32,7 +39,14 @@ class SongSearchVC(tk.Frame):
     def search_action(self):
         terms = [t.strip() for t in self.search.get().split(',')]
         print(terms)
-        data = self.app.searchSongAndPlaylists(terms)
+
+        data = [(1,Song(4, "Luckenbach Texas", 69)), (2, Song(88, "Allah's Plan", 420)), (3, Playlist(4, "My Cool Playlist", 69420))]
+
+        #data = self.app.searchSongAndPlaylists(terms)
+        for i in range(len(data)):
+            d = data[i]
+            md = d[1]  # MusicData
+            self.searchView.insert("",'end',iid=i, values=(md.ID, md.title, md.duration, md.__class__.__name__))
         print(data)
 
     def logout_action(self):
