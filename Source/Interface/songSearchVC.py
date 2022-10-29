e01f5e59856226aa703e1f812895ef30e1b86be2
Connor McLaughlan <cmclaughlan15@gmail.com>
2022-10-29 15:45:26 -0600

    UI search implemented

--- Source/Interface/songSearchVC.py
+++ Source/Interface/songSearchVC.py
@@ -1,20 +1,26 @@
 import tkinter as tk
 from tkinter import ttk
+from Application.application import *
 
 class SongSearchVC(tk.Frame):
     def __init__(self, app, parent=None):
+        self.app = app
+        self.parent = parent
+
         tk.Frame.__init__(self, parent)
         self.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
+
+        self.search = tk.StringVar()
+
         self.create_view()
 
     def create_view(self):
         searchView = ttk.Treeview(self, columns=('id', 'Title', 'Duration'))
         searchView.grid(column=0, row=0, columnspan=3)
 
-        search = tk.StringVar()  # member id
-        search_entry = tk.Entry(self, width=15, textvariable=search)
+        search_entry = tk.Entry(self, width=15, textvariable=self.search)
         search_entry.grid(column=0, row=1, columnspan=2, sticky=(tk.W, tk.E))
         
         search_button = tk.Button(self, text='Search', command=self.search_action)
@@ -24,7 +30,10 @@ class SongSearchVC(tk.Frame):
             child.grid_configure(padx=5, pady=5)
 
     def search_action(self):
-        pass
+        terms = [t.strip() for t in self.search.get().split(',')]
+        print(terms)
+        data = self.app.searchSongAndPlaylists(terms)
+        print(data)
 
     def logout_action(self):
         self.grid_forget()
