ea472b5112d3a9160319e083a8770633616863b2
Connor McLaughlan <cmclaughlan15@gmail.com>
2022-10-29 14:49:00 -0600

    Search results

--- Source/Interface/songSearchVC.py
+++ Source/Interface/songSearchVC.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 class SongSearchVC(tk.Frame):
     def __init__(self, app, parent=None):
@@ -9,12 +10,15 @@ class SongSearchVC(tk.Frame):
         self.create_view()
 
     def create_view(self):
+        searchView = ttk.Treeview(self, columns=('id', 'Title', 'Duration'))
+        searchView.grid(column=0, row=0, columnspan=3)
+
         search = tk.StringVar()  # member id
         search_entry = tk.Entry(self, width=15, textvariable=search)
-        search_entry.grid(column=0, row=0, columnspan=2, sticky=(tk.W, tk.E))
+        search_entry.grid(column=0, row=1, columnspan=2, sticky=(tk.W, tk.E))
         
         search_button = tk.Button(self, text='Search', command=self.search_action)
-        search_button.grid(column=2, row=0)
+        search_button.grid(column=2, row=1)
 
         for child in self.winfo_children(): 
             child.grid_configure(padx=5, pady=5)
