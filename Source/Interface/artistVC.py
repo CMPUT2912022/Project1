d2cc5e7133f1ecd31af95065c346bdaa0eab224d
Lvajtner <91029869+Lvajtner@users.noreply.github.com>
2022-11-02 20:12:12 -0600

    changing artist

--- Source/Interface/artistVC.py
+++ Source/Interface/artistVC.py
@@ -8,15 +8,16 @@ class ArtistVC(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
         self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
         self.rowconfigure(0, weight=1)
         self.create_view()
 
     def create_view(self):
         create_song_button = tk.Button(self, text='Create Song', command=self.create_song_action)
-        create_song_button.grid(column=0, row=0)
+        create_song_button.grid(column=0, row=0, sticky=(tk.W, tk.E))
 
         stats_button = tk.Button(self, text='Artist Stats', command=self.artist_stats_action)
-        stats_button.grid(column=1, row=0)
+        stats_button.grid(column=1, row=0, sticky=(tk.W, tk.E))
 
         for child in self.winfo_children(): 
             child.grid_configure(padx=5, pady=5)
